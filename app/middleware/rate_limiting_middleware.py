982472272f774eabb9686f4deea2ab5b115b0be1
Favour <georgefavour694@gmail.com>
2026-04-05 11:25:14 +0100

    middleware exception fix

--- app/middleware/rate_limiting_middleware.py
+++ app/middleware/rate_limiting_middleware.py
@@ -7,8 +7,11 @@ async def attach_user(request: Request, call_next):
 
     if auth and auth.startswith("Bearer "):
         token = auth.split(" ")[1]
-        decode_token = decode_jwt(token)
-        request.state.user_id = decode_token.get("sub")
+        try:
+            decode_token = decode_jwt(token)
+            request.state.user_id = decode_token.get("sub")
+        except Exception:
+            request.state.user_id = None
 
     response = await call_next(request)
     return response
